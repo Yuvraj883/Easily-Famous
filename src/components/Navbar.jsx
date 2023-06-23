fd388b5277ec2fff4d92d13d5bb855d00a419188
Yuvraj <kingyuvi88@gmail.com>
2023-06-23 20:59:41 +0530

    Added color gradient to the title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,10 @@ const Navbar = ()=>{
 
     return(
         <>
-        <nav className="bg-purple-400 flex flex-row justify-between items-center p-4">
-            <span className="font-bolder text-2xl">easilyFamous &#8482;</span>
+        <nav className="bg-purple-400 flex flex-row justify-between items-center text-white p-4">
+            <span className="font-extrabold text-2xl bg-gradient-to-r bg-clip-text text-transparent from-orange-500 to-red-500">easilyFamous &#8482;</span>
             <div>
-                <ul className="flex flex-row">
+                <ul className="flex flex-row font-bold">
                     <li className="mx-1 px-1 cursor-pointer"><Link to="/">Home</Link> </li>
                     <li className="mx-1 px-1 cursor-pointer "><Link to="offers">Offers</Link> </li>
                 </ul>
