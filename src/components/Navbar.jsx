a4f738bf1c2019a0f845b5e68591bc115e18de59
Yuvraj <kingyuvi88@gmail.com>
2023-06-23 19:40:25 +0530

    Updated styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,15 @@
 const Navbar = ()=>{
     return(
         <>
-        
+        <nav className="bg-purple-400 flex flex-row justify-between items-center p-4">
+            <span className="font-bolder text-2xl">easilyFamous &#8482;</span>
+            <div>
+                <ul className="flex flex-row">
+                    <li className="mx-1 px-1 cursor-pointer">Home</li>
+                    <li className="mx-1 px-1 cursor-pointer ">Offers</li>
+                </ul>
+            </div>
+        </nav>
         </>
     )
 }
