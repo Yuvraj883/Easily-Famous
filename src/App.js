06fbd132bc2fdc9da991055bf2d26e0f28ca8f81
Yuvraj <kingyuvi88@gmail.com>
2023-06-23 19:40:50 +0530

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import Offers from './pages/Offers';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
    <>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Navbar/>}>
+    <Route index element={<Home/>}/>
+    <Route path="offers" element={<Offers/>}/>
+    </Route>
+
+   </Routes>
+   </BrowserRouter>
+   
    <Home/>
    </>
   );
