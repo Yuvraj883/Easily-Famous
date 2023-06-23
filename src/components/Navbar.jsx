ca139c1a6dff13991e07dd0617455b154238adbb
Yuvraj <kingyuvi88@gmail.com>
2023-06-23 19:52:52 +0530

    Added routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,20 @@
+import {Link, Outlet} from 'react-router-dom';
 const Navbar = ()=>{
+
     return(
         <>
         <nav className="bg-purple-400 flex flex-row justify-between items-center p-4">
             <span className="font-bolder text-2xl">easilyFamous &#8482;</span>
             <div>
                 <ul className="flex flex-row">
-                    <li className="mx-1 px-1 cursor-pointer">Home</li>
-                    <li className="mx-1 px-1 cursor-pointer ">Offers</li>
+                    <li className="mx-1 px-1 cursor-pointer"><Link to="/">Home</Link> </li>
+                    <li className="mx-1 px-1 cursor-pointer "><Link to="offers">Offers</Link> </li>
                 </ul>
             </div>
         </nav>
+        <Outlet/>
         </>
+
     )
 }
 export default Navbar;
