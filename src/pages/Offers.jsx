cf43954bae602c5f69ca54640a11c3217f62fb52
Yuvraj <kingyuvi88@gmail.com>
2023-06-25 01:09:45 +0530

    Fixed Infinite Rerendering problem

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,31 +1,36 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import OfferCard from "../components/OfferCard";
 import axios from "axios";
 const Offers =()=>{
-    const [offers, setOffers] = useState('');
+    const [offers, setOffers] = useState(null);
 
     const key = '00b4e5c0f084ee04ceb376293f3be3c7';
     const baseURL = `https://indianprovider.com/api/v2`;
 
- useEffect (
-    ()=>{
-        function getOffers (){
-            axios.get(baseURL, {params:{
-                key:key,
-                action:'services'
-            }}).then((response)=>{
-                setOffers(response.data);
-                console.log(offers);
-            })
-        }
-        getOffers();
-    }, [baseURL]
- )
 
-  
+function getOffers (){
+    axios.get(baseURL, {params:{
+        key:key,
+        action:'services'
+    }}).then((response)=>{
+        setOffers(response.data);
+        console.log(offers);
+    })
+}
+
+useEffect(()=>{
+    getOffers();
+},[]);
+
     return(
         <>
-        Offers page
-        {/* <button className="bg-red-500 text-white p-8" onClick={getOffers()}>Call API</button> */}
+        {/* {
+
+            // offers.map((offer, index)=>(
+            //     <OfferCard props = {offer} key={index}/>
+            )
+            )
+        } */}
         </>
     )
 }
