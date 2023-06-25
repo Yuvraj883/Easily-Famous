36e92aa6bc3e36c35bb7ecc12b030c117a748988
Yuvraj <kingyuvi88@gmail.com>
2023-06-25 16:27:16 +0530

    Created the BlogCard for large and small screens

---
+++ src/components/BlogCard.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import {IoMdArrowDropdown, IoMdArrowDropup} from 'react-icons/io'
+const BlogCard = ({title, body})=>{
+    const [readMore, setReadMore] = useState(false);
+
+    const handleReadMore = ()=>{
+        setReadMore(!readMore);
+    }
+
+    return(
+        <>
+        <div className="flex flex-col justify-center items-center shadow-lg p-4 rounded-md mx-auto w-[90%] m-2 ">
+            <h1 className="text-xl font-bold ">{title}</h1>
+            <p className={readMore?'inline': 'line-clamp-3'}>
+                {body}
+            </p>
+            <p onClick={handleReadMore} className="text-blue-500 cursor-pointer ">{
+                !readMore?<IoMdArrowDropdown className="text-blue-500 text-2xl"/>: <IoMdArrowDropup className="text-blue-500 text-2xl "/>
+            }</p>
+        </div>
+        </>
+    )
+}
+export default BlogCard; 
