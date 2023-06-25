f0c69f94194eec2f723172b16251cdea3a20352a
Yuvraj <kingyuvi88@gmail.com>
2023-06-25 16:33:10 +0530

    Refactored code

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,43 +1,47 @@
 import { useEffect, useState } from "react";
 import OfferCard from "../components/OfferCard";
 import axios from "axios";
-const Offers =()=>{
+
+const key = '00b4e5c0f084ee04ceb376293f3be3c7';
+const baseURL = `https://indianprovider.com/api/v2`;
+const Offers = () => {
     const [offers, setOffers] = useState(null);
 
-    const key = '00b4e5c0f084ee04ceb376293f3be3c7';
-    const baseURL = `https://indianprovider.com/api/v2`;
+   
 
+    useEffect(() => {
 
-function getOffers (){
-    axios.get(baseURL, {params:{
-        key:key,
-        action:'services'
-    }}).then((response)=>{
-        console.log(response);
-
-        if(Array.isArray(response?.data)&& response?.data.length){
-            setOffers(response['data']);
-            console.log(offers);
+        function getOffers() {
+            axios.get(baseURL, {
+                params: {
+                    key: key,
+                    action: 'services'
+                }
+            }).then((response) => {
+    
+                if (Array.isArray(response?.data) && response?.data.length) {
+                    setOffers(response['data']);
+                    // console.log(offers);
+                }
+    
+            })
         }
-       
-    })
-}
-
-useEffect(()=>{
-    getOffers();
-},[]);
-    if(offers === null){
-        return <div>...loading</div>
+        getOffers();
+    }, []);
+    if (offers === null) {
+        return <div className="text-3xl mx-auto font-bolder flex justify-center items-center">loading...</div>
     }
-    return(
+    return (
         <>
-        {
-            
-            offers.map((offer, index)=>(
-                <OfferCard {...offer} key={index}/>
-            )
-            )
-        }
+            <div className="flex flex-col justify-center items-center p-4 overflow-hidden w-[100%]">
+                {
+
+                    offers.map((offer, index) => (
+                        <OfferCard {...offer} id={index} key={index} />
+                    )
+                    )
+                }
+            </div>
         </>
     )
 }
