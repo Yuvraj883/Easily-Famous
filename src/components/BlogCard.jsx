538545d771c751b9d839c029848689c099bcd659
Yuvraj <kingyuvi88@gmail.com>
2023-06-25 16:51:39 +0530

    Updated style for small screen devices

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -9,7 +9,18 @@ const BlogCard = ({title, body})=>{
 
     return(
         <>
-        <div className="flex flex-col justify-center items-center shadow-lg p-4 rounded-md mx-auto w-[90%] m-2 ">
+        {/* Design for large screens */}
+        <div className="md:flex hidden flex-col justify-center items-center shadow-lg p-4 rounded-md mx-auto w-[80%] m-2 ">
+            <h1 className="text-xl font-bold ">{title}</h1>
+            <p className={readMore?'inline': 'line-clamp-3'}>
+                {body}
+            </p>
+            <p onClick={handleReadMore} className="text-blue-500 cursor-pointer ">{
+                !readMore?<IoMdArrowDropdown className="text-blue-500 text-2xl"/>: <IoMdArrowDropup className="text-blue-500 text-2xl "/>
+            }</p>
+        </div>
+        {/* Design for small screens */}
+        <div className="md:hidden flex flex-col justify-center items-center shadow-lg p-4 rounded-md mx-auto w-[100%] m-2 ">
             <h1 className="text-xl font-bold ">{title}</h1>
             <p className={readMore?'inline': 'line-clamp-3'}>
                 {body}
