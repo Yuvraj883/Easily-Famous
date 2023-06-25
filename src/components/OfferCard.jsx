bf01d96e064548f86bc8213462fd11a6f2dddfa8
Yuvraj <kingyuvi88@gmail.com>
2023-06-25 14:00:59 +0530

    UI design completed for large screens

--- src/components/OfferCard.jsx
+++ src/components/OfferCard.jsx
@@ -1,9 +1,19 @@
-const OfferCard = ()=>{
+const OfferCard = ({name, rate, max, min, service, id})=>{
+    
     return(
         <>
-            <div>
-
+        {/* Design for large screens */}
+            <div className="bg-white flex flex-row justify-between items-center w-[80%] m-2 p-2 shadow-lg ">
+                <div>
+                 <span className="font-semibold text-xl">{id+1}.</span>   
+                <h1 className="font-semibold text-2xl inline ">{name}</h1>
+                <p className="font-semibold"> Min:{min} Max:{max}</p>
+                <h3 className="font-semibold">Price: &#8377; {rate}</h3>
+                </div>
+                <button className="bg-green-500 px-8 py-2 m-4 cursor-pointer text-white font-bold rounded-sm">Order</button>
+                
             </div>
+            
         </>
     )
 }
