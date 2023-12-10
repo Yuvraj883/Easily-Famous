48371f1175a37910dffd733e2d88e825ee70f7ec
Yuvraj <kingyuvi88@gmail.com>
2023-12-10 18:16:08 +0530

    Updated offers to only display service 2572

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -3,48 +3,44 @@ import OfferCard from "../components/OfferCard";
 import axios from "axios";
 
 const key = '00b4e5c0f084ee04ceb376293f3be3c7';
-// const baseURL = `https://indianprovider.com/api/v2`;
 const baseURL = 'http://localhost:3001/api/v2';
 
 const Offers = () => {
-    const [offers, setOffers] = useState(null);
+  const [offers, setOffers] = useState(null);
 
-   
-
-    useEffect(() => {
-
-        function getOffers() {
-            axios.get(baseURL, {
-                params: {
-                    key: key,
-                    action: 'services'
-                }
-            }).then((response) => {
-    
-                if (Array.isArray(response?.data) && response?.data.length) {
-                    setOffers(response['data']);
-                    // console.log(offers);
-                }
-    
-            })
+  useEffect(() => {
+    function getOffers() {
+      axios.get(baseURL, {
+        params: {
+          key: key,
+          action: 'services'
         }
-        getOffers();
-    }, []);
-    if (offers === null) {
-        return <div className="text-3xl mx-auto font-bolder flex justify-center items-center">loading...</div>
+      }).then((response) => {
+        if (Array.isArray(response?.data) && response?.data.length) {
+          setOffers(response.data);
+        }
+      })
     }
-    return (
-        <>
-            <div className="flex flex-col justify-center items-center p-4 overflow-hidden w-[100%]">
-                {
+    getOffers();
+  }, []);
 
-                    offers.map((offer, index) => (
-                        <OfferCard {...offer} id={index} key={index} />
-                    )
-                    )
-                }
-            </div>
-        </>
-    )
+  if (offers === null) {
+    return <div className="text-3xl mx-auto font-bolder flex justify-center items-center">loading...</div>
+  }
+
+  const filteredOffers = offers.filter(offer => offer.service === '2572');
+
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center p-4 overflow-hidden w-[100%]">
+        {
+          filteredOffers.map((offer, index) => (
+            <OfferCard {...offer} id={index} key={index} />
+          ))
+        }
+      </div>
+    </>
+  )
 }
+
 export default Offers;
