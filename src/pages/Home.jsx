0a92cf86bce2560e9cb76b6740e360bd8d0c332e
Yuvraj <kingyuvi88@gmail.com>
2023-12-19 23:23:41 +0530

    Added google analytics

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
 import blogs from "../blogs";
 import BlogCard from "../components/BlogCard";
+import ReactGA from 'react-ga';
+
 
 const Home = ()=>{
 
+   useEffect(()=>{
+        ReactGA.pageview(window.location.pathname);
+   },[])
     return(
         <>
       <div className="w-[100%] flex flex-col overflow-hidden justify-center items-center p-8"> 
