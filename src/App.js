73c38d4cb75ec1bdb06f0fbf70377c2c568e4fcc
Yuvraj <kingyuvi88@gmail.com>
2023-12-19 23:23:13 +0530

    Initialized Google Analytics

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 // import Offers from './pages/Offers';
+import ReactGA from 'react-ga';
 import NewOffers from './pages/NewOffers';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+const Measurement_ID = 'G-678K43VV21'; 
+ReactGA.initialize(Measurement_ID);
 
 function App() {
   return (
