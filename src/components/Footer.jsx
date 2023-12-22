fdc97cf8d8f86ad758637aff110f4adbb946f726
Yuvraj <kingyuvi88@gmail.com>
2023-12-23 01:59:04 +0530

    Created Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = ()=>{
+    return(
+        <>
+        <footer className="md:bg-purple-600 bg-red-600 text-white m-auto py-4 w-screen fixed bottom-0">
+            <span className="flex justify-center ">For more offers visit  <u><a className="cursor-pointer pl-2"  href="https://indianprovider.com/"> indianprovider.com</a></u>💌</span>
+        </footer>
+        </>
+    )
+}
+export default Footer; 
