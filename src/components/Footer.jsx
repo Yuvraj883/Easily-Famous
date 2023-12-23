33f7647d3073cae1ebbb5cca00e49ae219ec2fb7
Yuvraj <kingyuvi88@gmail.com>
2023-12-23 23:09:37 +0530

    Updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,35 @@
-const Footer = ()=>{
-    return(
-        <>
-        <footer className="md:bg-purple-600 bg-red-600 text-white m-auto py-4 w-screen fixed bottom-0">
-            <span className="flex justify-center ">For more offers visit  <u><a className="cursor-pointer pl-2"  href="https://indianprovider.com/"> indianprovider.com</a></u>💌</span>
-        </footer>
-        </>
-    )
-}
-export default Footer; 
+const Footer = () => {
+    return (
+      <footer className="md:bg-purple-600 bg-red-600 text-white py-4 w-screen fixed bottom-0">
+        <div className="flex justify-center items-center flex-col h-full">
+          <span className="mb-2">
+            For more offers, visit{" "}
+            <a
+              className="cursor-pointer pl-2 text-blue-300 hover:underline"
+              href="https://indianprovider.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              indianprovider.com
+            </a>
+            💌
+          </span>
+          <p className="mb-2">
+            Made with Love by{" "}
+            <a
+              className="px-1 cursor-pointer text-blue-300 hover:underline"
+              href="https://github.com/Yuvraj883"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Yuvraj Singh
+            </a>
+          </p>
+          <span className="mb-2">All rights reserved &#169; </span>
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
